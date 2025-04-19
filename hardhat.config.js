88aef12daad27f4370d2c3bf4e094a482eebdd40
wasim8g <wasimakaram360@gmail.com>
2025-04-19 18:51:30 +0530

    Add hardhat.config.js 1745068890875

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    arbitrumTestnet: {
+      url: process.env.ARBITRUM_TESTNET_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+  },
+};
